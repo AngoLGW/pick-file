e195b59be882d8c5c7b0a8423a9cab74f713d43e
ango <lingw9067@fxiaoke.com>
2022-02-28 10:39:21 +0800

    feat: 使用精简版

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-function pickFile(params = {}) {
-  let { multiple, accept, minNum, maxNum, maxSize } = params;
-
+function pickFile() {
   return new Promise((resolve, reject) => {
     let input = document.createElement("input");
     input.setAttribute("type", "file");
@@ -14,21 +12,6 @@ function pickFile(params = {}) {
       if (multiple) {
         let files = e.path[0].files;
         files = Array.from(files);
-        if (minNum && files.length < minNum) {
-          reject(`选择文件数量不能小于${minNum}`);
-        }
-        if (maxNum && files.length > maxNum) {
-          reject(`选择文件数量不能大于${maxNum}`);
-        }
-        if (maxSize) {
-          let totle = files.reduce((pre, cur) => {
-            return pre + cur.size;
-          }, 0);
-          //最大文件大小默认传入单位为KB
-          if (maxSize * 1000 < totle) {
-            reject(`选择文件总大小不能大于 ${maxSize} KB`);
-          }
-        }
         resolve(
           files.map((file) => {
             return {
@@ -43,10 +26,6 @@ function pickFile(params = {}) {
         );
       } else {
         let file = e.path[0].files[0];
-        //最大文件大小默认传入单位为KB
-        if (maxSize && maxSize * 1000 < file.size) {
-          reject(`选择文件大小不能大于 ${maxSize} KB`);
-        }
         resolve({
           file,
           property: {
